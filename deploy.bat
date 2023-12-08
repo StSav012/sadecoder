1963abf586003775ddf94fd0c7dde3b4aa1d80f6
StSav012 <stsav012@gmail.com>
2023-12-08 12:40:29 +0300

    Refine the deployment shell scripts

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,14 @@
-python -m nuitka --onefile --windows-icon-from-ico=icon.png --disable-console --enable-plugin=tk-inter --deployment --output-filename=sadecoder main.py
+python -m nuitka ^
+ --onefile ^
+ --deployment ^
+ --windows-icon-from-ico=icon.png ^
+ --disable-console ^
+ --enable-plugin=tk-inter ^
+ --nofollow-import-to=email ^
+ --nofollow-import-to=decimal ^
+ --nofollow-import-to=hashlib ^
+ --nofollow-import-to=bz2 ^
+ --nofollow-import-to=lzma ^
+ --nofollow-import-to=select ^
+ --output-filename=sadecoder ^
+ main.py
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
-/bin/env python -m nuitka --onefile --macos-create-app-bundle --macos-app-icon=icon.png --disable-console --enable-plugin=tk-inter --deployment --output-filename=sadecoder main.py
+/bin/env python -m nuitka \
+ --onefile \
+ --deployment \
+ --macos-create-app-bundle --macos-app-icon=icon.png \
+ --disable-console \
+ --enable-plugin=tk-inter \
+ --nofollow-import-to=email \
+ --nofollow-import-to=decimal \
+ --nofollow-import-to=hashlib \
+ --nofollow-import-to=bz2 \
+ --nofollow-import-to=lzma \
+ --nofollow-import-to=select \
+ --nofollow-import-to=csv \
+ --nofollow-import-to=statictics \
+ --output-filename=sadecoder \
+ main.py
